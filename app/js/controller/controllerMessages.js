4e686555462fc5fa3ad109a4b95500940456dad0
Everton de Castro <evertoncastro.sp@gmail.com>
2016-02-13 17:47:28 -0200

    Add tab on massage session

--- app/js/controller/controllerMessages.js
+++ app/js/controller/controllerMessages.js
@@ -4,10 +4,9 @@
 app.controller('MessagesCtrl', function ($scope, $location, serviceMessage) {
 
     $scope.showMessage = undefined;
-
-    $scope.registerMessage = function(message){
-        serviceMessage.registerMessage(message);
-    };
+    $scope.showTabMessages = true;
+    $scope.tabTitle = 'MINHAS MENSAGENS';
+    $scope.buttonTitle = 'Criar mensagem';
 
     $scope.init = function(){
         serviceMessage.loadMessageByUser().then(
@@ -17,11 +16,27 @@ app.controller('MessagesCtrl', function ($scope, $location, serviceMessage) {
         );
     };
 
+    $scope.registerMessage = function(message){
+        serviceMessage.registerMessage(message);
+    };
+
     $scope.openMessage = function(index){
         if($scope.showMessage || $scope.showMessage==0){
             $scope.showMessage = undefined;
         }else{
             $scope.showMessage = index;
+        }
+    };
+
+    $scope.changeTab = function(){
+        if($scope.showTabMessages){
+            $scope.showTabMessages = false;
+            $scope.tabTitle = 'CRIAR MENSAGEM';
+            $scope.buttonTitle = 'Voltar';
+        }else{
+            $scope.showTabMessages = true;
+            $scope.tabTitle = 'MINHAS MENSAGENS';
+            $scope.buttonTitle = 'Criar mensagem';
         }
     };
 
